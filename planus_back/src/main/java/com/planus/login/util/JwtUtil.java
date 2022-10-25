e5a2f6a0943d19ea415f0e63ab68a6ec96644fd4
김지건 <jigun21@naver.com>
2022-10-25 17:31:33 +0900

    chore: "conflict 해결?"

---
+++ planus_back/src/main/java/com/planus/login/util/JwtUtil.java
@@ -0,0 +1,52 @@
+package com.planus.login.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String createToken(long userId, String nickname, String email){
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+
+        Map<String, Object> payloads = new HashMap<>();
+        payloads.put("userId", String.valueOf(userId));
+        payloads.put("name", nickname);
+        payloads.put("email", email);
+        //일단은 1달로 설정
+        Long expiredTime = 1000 * 60*60*30L;
+
+        Date ext = new Date();
+        ext.setTime(ext.getTime()+expiredTime);
+
+        String jwt = Jwts.builder()
+                .setHeader(headers)
+                .setClaims(payloads)
+                .setSubject("Test")
+                .setExpiration(ext)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+        return jwt;
+    }
+
+    public long getUserIdFromToken(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        return Long.parseLong((String) claims.get("userId"));
+    }
+}
