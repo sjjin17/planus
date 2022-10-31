1839838357abad297c95e2e495644aa2f6601a26
나경 <leenak0@naver.com>
2022-10-31 11:08:34 +0900

    feat: userId token에서 가져오기, 참가자 등록, 인원제한

--- planus_back/src/main/java/com/planus/trip/dto/TripInfoResDTO.java
+++ planus_back/src/main/java/com/planus/trip/dto/TripInfoResDTO.java
@@ -12,15 +12,17 @@ public class TripInfoResDTO {
     private long admin;
     private String startDate;
     private long period;
+    private int memberOrAdmin;
     private boolean complete;
     private String imageUrl;
     private List<TripAreaDTO> tripArea;
 
-    public TripInfoResDTO(long tripId, long admin, String startDate, long period, boolean complete, String imageUrl, List<TripAreaDTO> tripArea) {
+    public TripInfoResDTO(long tripId, long admin, String startDate, long period, int memberOrAdmin, boolean complete, String imageUrl, List<TripAreaDTO> tripArea) {
         this.tripId = tripId;
         this.admin = admin;
         this.startDate = startDate;
         this.period = period;
+        this.memberOrAdmin = memberOrAdmin;
         this.complete = complete;
         this.imageUrl = imageUrl;
         this.tripArea = tripArea;
--- planus_back/src/main/java/com/planus/trip/service/MemberService.java
+++ planus_back/src/main/java/com/planus/trip/service/MemberService.java
@@ -5,5 +5,7 @@ import com.planus.trip.dto.MemberResDTO;
 import java.util.List;
 
 public interface MemberService {
+
+    long addMember(String token, long tripId);
     List<MemberResDTO> findMemberList(long tripId);
 }
