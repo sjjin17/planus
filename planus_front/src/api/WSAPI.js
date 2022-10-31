c8241de986dd32df914dc4c4bcce215fbfbb6f26
s-jun <joon8445@naver.com>
2022-10-31 13:58:46 +0900

    feat: 버킷리스트 추가 웹소켓 (BE,FE)

--- planus_front/src/api/WSAPI.js
+++ planus_front/src/api/WSAPI.js
@@ -1,8 +1,8 @@
 import SockJS from "sockjs-client";
 import Stomp from "webstomp-client";
 
-// const WS_SERVER_URL = "http://localhost:8080/planus/ws";
-const WS_SERVER_URL = "https://k7a505.p.ssafy.io/planus/ws";
+const WS_SERVER_URL = "http://localhost:8080/planus/ws";
+// const WS_SERVER_URL = "https://k7a505.p.ssafy.io/planus/ws";
 
 const WSAPI = {
   socket: null,
@@ -16,7 +16,7 @@ const WSAPI = {
     this.stomp.connect(
       {},
       () => {
-        this.stomp.subscribe("/topic/planus/{trip_id}", callback);
+        this.stomp.subscribe("/topic/planus/{tripId}", callback);
         this.enter({ tripId, userName });
       },
       (error) => {
@@ -30,6 +30,26 @@ const WSAPI = {
   },
   chat(message) {
     this.stomp.send("/app/chat", JSON.stringify(message));
+  },
+  addBucket(tripId, place, address, lat, lng) {
+    let bucket = {
+      tripId: tripId,
+      place: place,
+      address: address,
+      lat: lat,
+      lng: lng,
+    };
+    this.stomp.send("/app/addBucket", JSON.stringify(bucket));
+  },
+  delBucket(tripId, place, address, lat, lng) {
+    let bucket = {
+      tripId: tripId,
+      place: place,
+      address: address,
+      lat: lat,
+      lng: lng,
+    };
+    this.stomp.send("/app/delBucket", JSON.stringify(bucket));
   },
 };
 
