3b2b7a176b9f41f86c851cf2c81cb9e15fa456c3
s-jun <joon8445@naver.com>
2022-11-01 09:26:29 +0900

    feat: addPlan -> addTimetable, setPlan으로 변경

--- planus_front/src/components/recommend/RecommendPlaceTab.vue
+++ planus_front/src/components/recommend/RecommendPlaceTab.vue
@@ -5,7 +5,7 @@
       :key="recommendPlace.recommendId"
       :recommendPlace="recommendPlace"
       @addBucket="addBucket"
-      @addPlan="addPlan"
+      @addTimetable="addTimetable"
     ></recommend-place-card>
     <v-container>
       <v-row justify="center">
@@ -60,8 +60,8 @@ export default {
     addBucket(place, address, lat, lng) {
       this.$emit("addBucket", place, address, lat, lng);
     },
-    addPlan(hours, minutes, place, lat, lng) {
-      this.$emit("addPlan", hours, minutes, place, lat, lng);
+    addTimetable(hours, minutes, place, lat, lng) {
+      this.$emit("addTimetable", hours, minutes, place, lat, lng);
     },
   },
 };
