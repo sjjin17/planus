e494fcf43f93eeb35633138eac5e45b2a0d5e66c
SSAFY <danijde71@naver.com>
2022-11-02 09:56:29 +0900

    fix: FE baseurl 관련 수정

--- planus_front/src/api/WSAPI.js
+++ planus_front/src/api/WSAPI.js
@@ -1,8 +1,8 @@
 import SockJS from "sockjs-client";
 import Stomp from "webstomp-client";
 
-const WS_SERVER_URL = "http://localhost:8080/planus/ws";
-// const WS_SERVER_URL = "https://k7a505.p.ssafy.io/planus/ws";
+// const WS_SERVER_URL = "http://localhost:8080/planus/ws";
+const WS_SERVER_URL = "https://k7a505.p.ssafy.io/planus/ws";
 
 const WSAPI = {
   socket: null,
