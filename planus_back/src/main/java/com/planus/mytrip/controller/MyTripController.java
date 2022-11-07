ed20cebd713654a0a77cc8375add4c94fd3e225d
정다은 <danijde71@naver.com>
2022-11-07 22:39:16 +0900

    feat: 내가 속한 모든 여행방 api

--- planus_back/src/main/java/com/planus/mytrip/controller/MyTripController.java
+++ planus_back/src/main/java/com/planus/mytrip/controller/MyTripController.java
@@ -1,6 +1,7 @@
 package com.planus.mytrip.controller;
 
 import com.planus.mytrip.dto.MyTripListResDTO;
+import com.planus.mytrip.dto.MyTripResDTO;
 import com.planus.mytrip.service.MyTripService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -25,6 +27,12 @@ public class MyTripController {
     @GetMapping("/shared")
     public ResponseEntity<MyTripListResDTO> getSharedTripList(@RequestHeader String Authorization, @PageableDefault(size = 6, sort = "createTime", direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok().body(mytripService.getSharedTripList(Authorization, pageable));
+    }
+
+    // 내가 참가자 또는 방장인 모든 여행 정보(최근 작성일 순)
+    @GetMapping("/all")
+    public ResponseEntity<List<MyTripResDTO>> getMyTripList(@RequestHeader String Authorization) {
+        return ResponseEntity.ok().body(mytripService.getMyTripList(Authorization));
     }
 
     @DeleteMapping
