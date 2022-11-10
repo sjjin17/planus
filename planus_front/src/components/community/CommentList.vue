877bcbc649bb5fbb63c9d68d25a2136a3340572b
나경 <leenak0@naver.com>
2022-11-10 17:50:41 +0900

    feat : 댓글 입력 구현

--- planus_front/src/components/community/CommentList.vue
+++ planus_front/src/components/community/CommentList.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <input type="text" maxlength="200" />
-    <v-btn>입력</v-btn>
+    <v-row align="center" justify="center">
+      <v-col cols="8">
+        <v-textarea
+          v-model="commentInput"
+          counter="20"
+          placeholder="댓글을 입력하세요"
+        ></v-textarea>
+      </v-col>
+      <v-col cols="1">
+        <v-btn @click="goComment()">입력</v-btn>
+      </v-col>
+    </v-row>
     <div v-for="(comment, i) in commentList" :key="i">
       {{ comment.name }}: {{ comment.content }} ({{ comment.regDate }})
       <button v-if="userId == comment.userId">수정</button>
@@ -41,6 +51,7 @@ export default {
       currentPage: 1,
       commentList: [],
       totalPage: 0,
+      commentInput: "",
     };
   },
   async created() {
@@ -54,11 +65,13 @@ export default {
     },
   },
   methods: {
+    async addComment() {
+      this.res = await api.addComment(this.articleId, this.commentInput);
+    },
     async getCommentList(page) {
       this.res = await api.getArticleComment(this.articleId, page - 1);
       this.commentList = this.res.commentPage.commentList;
       this.totalPage = this.res.commentPage.totalPage;
-      console.log("댓글 페이지수: " + this.totalPage);
     },
     async delComment(commentId) {
       this.res = await api.delComment(commentId);
@@ -70,6 +83,11 @@ export default {
         this.nickname = decode.nickname;
         this.userId = decode.userId;
       }
+    },
+    async goComment() {
+      await this.addComment();
+      this.commentInput = "";
+      this.getCommentList(1);
     },
   },
 };
