0bebea656a459ef303586fae616db128a66614aa
김승준 <joon8445@naver.com>
2022-11-13 13:50:47 +0900

    feat: 마이페이지 커뮤니티 관련 추가

--- planus_front/src/components/mypage/MyCommentList.vue
+++ planus_front/src/components/mypage/MyCommentList.vue
@@ -1,7 +1,60 @@
-<template><div></div></template>
+<template>
+  <div>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">댓글</th>
+            <th class="text-left">수정시간</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="comment in commentList" :key="comment.commentId">
+            <td>{{ comment.content }}</td>
+            <td>{{ comment.regDate }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-row justify="center" class="pagenation-bar">
+      <v-pagination
+        v-model="currentPage"
+        class="my-4"
+        :length="totalPage"
+        color="#4a8072"
+      ></v-pagination>
+    </v-row>
+  </div>
+</template>
 
 <script>
-export default {};
+import API from "@/api/RESTAPI";
+const api = API;
+export default {
+  data() {
+    return {
+      commentList: [],
+      currentPage: 1,
+      totalPage: 1,
+    };
+  },
+  async created() {
+    await this.getMyCommentList();
+  },
+  watch: {
+    currentPage() {
+      this.getMyCommentList();
+    },
+  },
+  methods: {
+    async getMyCommentList() {
+      await api.getMyComment(this.currentPage - 1).then((res) => {
+        this.commentList = res.commentPage.commentList;
+        this.totalPage = res.commentPage.totalPage;
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
--- planus_front/src/components/mypage/MyLikeList.vue
+++ planus_front/src/components/mypage/MyLikeList.vue
@@ -1,7 +1,64 @@
-<template><div></div></template>
+<template>
+  <div>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">제목</th>
+            <th class="text-left">좋아요</th>
+            <th class="text-left">조회수</th>
+            <th class="text-left">수정시간</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="article in articleList" :key="article.articleId">
+            <td>{{ article.title }}</td>
+            <td>{{ article.likeCount }}</td>
+            <td>{{ article.hits }}</td>
+            <td>{{ article.regDate }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-row justify="center" class="pagenation-bar">
+      <v-pagination
+        v-model="currentPage"
+        class="my-4"
+        :length="totalPage"
+        color="#4a8072"
+      ></v-pagination>
+    </v-row>
+  </div>
+</template>
 
 <script>
-export default {};
+import API from "@/api/RESTAPI";
+const api = API;
+export default {
+  data() {
+    return {
+      articleList: [],
+      currentPage: 1,
+      totalPage: 1,
+    };
+  },
+  async created() {
+    await this.getLikeList();
+  },
+  watch: {
+    currentPage() {
+      this.getLikeList();
+    },
+  },
+  methods: {
+    async getLikeList() {
+      await api.getMyLikedArticle(this.currentPage - 1).then((res) => {
+        this.articleList = res.articleList;
+        this.totalPage = res.totalPage;
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
